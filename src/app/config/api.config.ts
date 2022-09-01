3087156f3277416e8101077ebe885abbe6040b18
Victor Icoma <31005408+victoricoma@users.noreply.github.com>
2022-09-01 15:18:20 -0300

    Correção
    Ajuste de endereços

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,6 +1,4 @@
 export const API_CONFIG = {
-    // baseUrl: "https://helpr-backendv1.herokuapp.com",
-    // serviceUrl: "https://helpr-backendv1.herokuapp.com/service"
-    baseUrl: "http://localhost:8080",
-    serviceUrl: "http://localhost:8080/service"
+    baseUrl: "https://helpr-backendv1.herokuapp.com",
+    serviceUrl: "https://helpr-backendv1.herokuapp.com/service"
 }
