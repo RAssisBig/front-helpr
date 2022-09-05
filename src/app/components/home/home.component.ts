085ed20537f98916bf77e7f9696ad6b4d12f9d39
AndersonGama31 <andersongama31@gmail.com>
2022-09-05 16:54:56 -0300

    Implementação de PWA ao helpr

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public lat!: number;
+  public lon!: number;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient, private swPush: SwPush
+    ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getUserLocation();
+    this.getPushNotifications();
   }
 
+  getPushNotifications() {
+    this.swPush
+      .requestSubscription({
+        serverPublicKey:
+          'BA5ij5aNkvDBwo-Bgle58gUwTLwvrjTo8GATPQrC3Ocm2igx5eqen7y7us3R5K1FGjxpNiFogfXbxbsUN3b2SLk',
+      })
+      .then(() => {
+        console.log('Usuário permitiu notificações.');
+      })
+      .catch((error) => {
+        console.log(
+          'Usuário recusou ou o navegador não suporta a notificação',
+          error
+        );
+      });
+  }
+
+  getUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.lon = position.coords.longitude;
+        console.log(this.lat, this.lon);
+      });
+    } else {
+      console.log('Usuário não permitiu compartilhar sua localização!');
+    }
+  }
 }
