715b551fd98baeaf92450758a963d0e12e0e65ae
gabs-024 <dev.gabsferreira@gmail.com>
2022-09-08 17:26:19 -0300

    Criação do servço de futuros candidatos

--- src/app/components/login/children/trabalhe-conosco-form/trabalhe-conosco-form.component.ts
+++ src/app/components/login/children/trabalhe-conosco-form/trabalhe-conosco-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { TecnicosFuturos } from 'src/app/models/tecnicos-futuros';
+import { TecnicosFuturosService } from 'src/app/services/tecnicosfuturos.service';
+
 
 @Component({
   selector: 'app-trabalhe-conosco-form',
@@ -10,10 +12,13 @@ import { TecnicosFuturos } from 'src/app/models/tecnicos-futuros';
 export class TrabalheConoscoFormComponent implements OnInit {
   public formCadastro: FormGroup;
   public formBuilder: FormBuilder;
+  private service: TecnicosFuturosService ;
 
-  isLinear = false;
-  constructor(formBuilder: FormBuilder) {
-    this.formBuilder = formBuilder
+
+  isLinear = true;
+  constructor(formBuilder: FormBuilder, service: TecnicosFuturosService) {
+    this.formBuilder = formBuilder;
+    this.service = service;
     this.formCadastro = formBuilder.group({
       nome: [""],
       formacao: [""],
@@ -23,11 +28,16 @@ export class TrabalheConoscoFormComponent implements OnInit {
     })
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
+
   send() {
-    let cadastro: TecnicosFuturos = this.formCadastro.value
-    console.log(cadastro)
+    let cadastro: TecnicosFuturos = this.formCadastro.value;
+    this.service.insert(cadastro).subscribe({
+      next: response => {
+      }
+    });
+    console.log(cadastro);
     alert("enviado")
   }
 }
+
