5885b4c1ce9ceac12d8778e86e34627ae8442034
KDalcantara <kelvin.duartework@gmail.com>
2022-09-08 23:01:50 -0300

    Adição de dialog de logout ao clicar em sair

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -47,7 +47,7 @@ const routes: Routes = [
   },
   {
     path: "logout",
-    component: LoginComponent,
+    component: ExitComponent,
     canActivate: [AuthGuard]
   },
   {
--- src/app/components/nav-bar/nav-bar.module.ts
+++ src/app/components/nav-bar/nav-bar.module.ts
@@ -5,12 +5,14 @@ import { MaterialModule } from 'src/app/shared/material/material.module';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { FooterModule } from '../footer/footer.module';
+import { LogoutDialogComponent } from './logout-dialog/logout-dialog.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    LogoutDialogComponent
   ],
   imports: [
     CommonModule,
